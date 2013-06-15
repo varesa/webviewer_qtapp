2ed6c3da747b88cd4fe97fdc503004fd80426eeb
Esa Varemo <esa@kuivanto.fi>
2013-06-15 23:20:25 +0300

    Commenting

--- main.cpp
+++ main.cpp
@@ -7,8 +7,13 @@
 
 int main(int argc, char *argv[])
 {
+    
+    // Initialize app
+
     QApplication app(argc, argv);
     app.setOverrideCursor(Qt::BlankCursor);
+
+    // Initialize window
 
     QWidget *window = new QWidget();
     window->setCursor(Qt::BlankCursor);
@@ -19,17 +24,23 @@ int main(int argc, char *argv[])
     layout->setSpacing(0);
     layout->setContentsMargins(0,0,0,0);
 
+    // Initialize web browser
+
     QWebView *webview = new QWebView();
     webview->setUrl(QUrl("http://www.google.com"));
     webview->page()->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
     webview->page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
 
-    //window->setWindowFlags(Qt::FramelessWindowHint);
+
+    // Open window
+
     window->resize(800, 1000);
     window->setLayout(layout);
     layout->addWidget(webview);
     webview->show();
     window->showMaximized();
+
+    // Exec...
 
     return app.exec();
 }
